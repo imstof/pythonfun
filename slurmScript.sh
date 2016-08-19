e8749ba10944d4af0775ecb2864decd6c9097b50
imstof <cehnstrom@techsquare.com>
2016-08-19 13:22:27 -0400

    renamed, tested

--- slurmScript.sh
+++ slurmScript.sh
@@ -1,20 +1,19 @@
 #!/bin/bash
 #Try to run python script on slurm
 
-#SBATCH -N1 -n1 --mem-per-cpu=100M -t01:15:00 
+#SBATCH --mem-per-cpu=100M
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=cehnstrom@techsquare.com
 #SBATCH -J testy
 
+echo ""
 date +"%D-%T"
 hostname
 domainname
 echo "I am: `whoami`"
 echo "I am at `pwd`"
 echo ""
-echo ""
 ./$1
-echo ""
 echo ""
 echo "Script Complete"
 date +"%D-%T"
