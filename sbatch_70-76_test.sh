4277bca23ea71f753616290cea0faa764c8034d1
imstof <imstof@login003.cm.cluster>
2019-03-18 16:45:17 -0400

    Mon Mar 18 16:45:17 EDT 2019

---
+++ sbatch_70-76_test.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+#SBATCH --mem-per-cpu=1000M
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=cehnstrom@techsquare.com
+#SBATCH --reservation=centos7_test
+#SBATCH -p sched_mem1TB_centos7
+
+echo ""
+date +"%D-%T"
+hostname
+domainname
+echo "I am: `whoami`"
+echo "I am at `pwd`"
+echo ""
+./$1
+echo ""
+echo "Script Complete"
+date +"%D-%T"
